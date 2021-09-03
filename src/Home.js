6c3247d680f95ef4c648b67e1067717b678b6e7c
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-03 12:02:44 -0400

    generate md5 for api request

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import md5 from 'md5';
 
 export const Home = () => {
+
+    const [characters, setCharacters] = useState(null);
+    const [comics, setComics] = useState(null);
+    const [hash] = useState(md5(1+process.env.REACT_APP_PRIVATE_KEY+process.env.REACT_APP_PUBLIC_KEY));
+
     return (
-        <div>
-            Home
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    
+                </div>
+            </div>
         </div>
     )
 }
