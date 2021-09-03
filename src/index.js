d2e8e4fec7e1e09d47bd017ab0bbb7524bf3bc7d
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-03 10:35:18 -0400

    import bootstrap, fix bad import on package.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import reactDom from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import { Home } from "./Home";
 
 
