74c0816d42a258d226e28bd1cf1a02bb0900e8b2
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-03 15:55:00 -0400

    complete demo with characters

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,7 @@ export const Home = () => {
     const [comics, setComics] = useState(null);
     const [hash] = useState(md5(1+process.env.REACT_APP_PRIVATE_KEY+process.env.REACT_APP_PUBLIC_KEY));
     const [selected, setSelected] = useState(null);
+    const [active, setActive] = useState(0)
 
     
         useEffect(() => {
@@ -15,11 +16,11 @@ export const Home = () => {
             getComics();
         }, [])
 
-    function getCharacters() {
-        fetch(`${process.env.REACT_APP_API_URL}/v1/public/characters?ts=1&apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=${hash}`)
+    function getCharacters( offset = 0, limit = 20 ) {
+        fetch(`${process.env.REACT_APP_API_URL}/v1/public/characters?ts=1&apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=${hash}&limit=${limit}&offset=${offset}`)
             .then( resp => resp.json())
             .then( data => {
-                setCharacters(data)
+                setCharacters(data);
             });
     }
 
@@ -39,6 +40,74 @@ export const Home = () => {
             <div className="row">
                 <div className="col-md-12">
                     <h1>Home</h1>
+                </div>
+                {/* <div className="col-md-12">
+                    {
+                        !!characters &&
+                        "Total páginas: " + Math.ceil(characters.data.total / characters.data.limit)
+                    }
+                </div> */}
+                <div className="col-md-12">
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        <li className="page-item">
+                            <a className="page-link" href="#">
+                                Previous
+                            </a>
+                        </li>
+                        <li className={"page-item" + (active === 0 ? " active" : "")}>
+                            <a className="page-link" href="#" onClick={ (e) => {
+                                e.preventDefault();
+                                getCharacters(0 * characters.data.limit);
+                                setActive(0);
+                            }}>
+                                1
+                            </a>
+                        </li>
+                        <li className={"page-item" + (active === 1 ? " active" : "")}>
+                            <a className="page-link" href="#" onClick={ (e) => {
+                                e.preventDefault();
+                                getCharacters(1 * characters.data.limit);
+                                setActive(1);
+                            }}>
+                                2
+                            </a>
+                        </li>
+                        <li className={"page-item" + (active === 2 ? " active" : "")}>
+                            <a className="page-link" href="#" onClick={ (e) => {
+                                e.preventDefault();
+                                getCharacters(2 * characters.data.limit);
+                                setActive(2);
+                            }}>
+                                3
+                            </a>
+                        </li>
+                        <li className={"page-item" + (active === 3 ? " active" : "")}>
+                            <a className="page-link" href="#" onClick={ (e) => {
+                                e.preventDefault();
+                                getCharacters(3 * characters.data.limit);
+                                setActive(3);
+                            }}>
+                                4
+                            </a>
+                        </li>
+                        <li className={"page-item" + (active === 4 ? " active" : "")}>
+                            <a className="page-link" href="#" onClick={ (e) => {
+                                e.preventDefault();
+                                getCharacters(4 * characters.data.limit);
+                                setActive(4);
+                            }}>
+                                5
+                            </a>
+                        </li>
+                        <li className="page-item">
+                            <a className="page-link" href="#">
+                                Next
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
                 </div>
             </div>
             <div className="row">
